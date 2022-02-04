d29c67615f96cbb8f0f2351e8a907f75bfefcaed
Jason Kim <xmflak20@gmail.com>
2022-02-05 00:40:05 +0900

    [Bronze II] Title: 벌집, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/2292.벌집/벌집.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+let input = Int(readLine()!)!
+
+let a = input
+var sum = 1
+var i = 0
+while true {
+    sum = sum + 6 * i
+    if a <= sum {
+        print(i + 1)
+        break
+    }
+
+    i = i + 1
+}
