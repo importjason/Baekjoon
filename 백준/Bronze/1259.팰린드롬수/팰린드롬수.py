3ae08195cf240c2729ae7d041d571fa22afb2cf4
Jason Kim <xmflak20@gmail.com>
2022-02-05 15:47:23 +0900

    [Bronze I] Title: 팰린드롬수, Time: 64 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Bronze/1259.팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259.팰린드롬수/팰린드롬수.py
@@ -1,11 +1,7 @@
-while True :
-  a = input()
-  if int(a) == 0 :
-    break
-  n = len(a)
-  if ((n % 2 == 0) and (a[n//2-1::-1] == a[(n//2):])) :
-    print('yes')
-  elif (n % 2 == 1) and (a[(n-1)//2::-1] == a[(n-1)//2:]) :
-    print('yes')
-  else :
-    print('no')
+import sys
+
+while True:
+  a = sys.stdin.readline().rstrip()
+  if a == '0':
+        break
+  print('yes' if a == a[::-1] else 'no')
