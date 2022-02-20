930c4a91a9bb357239b1f05e11860e69dced6a0e
Jason Kim <xmflak20@gmail.com>
2022-02-20 13:37:48 +0900

    Update 소수 찾기.py

--- 백준/Silver/1978.소수 찾기/소수 찾기.py
+++ 백준/Silver/1978.소수 찾기/소수 찾기.py
@@ -2,17 +2,17 @@ n = int(input())
 lst = list(map(int,input().split()))
 ans = 0
 
-def pncheck(num) :
-  div = 0
+def pncheck(num) :    # 소수인지 확인하는 함수
+  div = 0    # num의 약수의 개수
   for i in range(1, num+1) :
-    if num % i == 0 :
+    if num % i == 0 :    # num을 1부터 num까지 하나씩 나눠보고, 약수 개수 확인
       div += 1
   if div == 2 :
-    return(1)
+    return(1)    # 약수의 개수 == 2 면 소수이므로 return(1)
   else :
     return(0)
 
 for i in lst :
-  ans += pncheck(i)
+  ans += pncheck(i)    # lst의 숫자가 소수이면 ans에 1 추가, 아니면 0 추가. 결국 소수의 개수가 ans에 담긴다.
 
 print(ans)
